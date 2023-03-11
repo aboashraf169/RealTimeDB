767e824760241f960ca2537a7015786deb3b0ee2
aboashraf169 <abo.ashraf169@gmail.com>
2023-03-11 22:47:38 +0200

    FirstCommit

---
+++ app/src/main/java/com/example/realtime_db/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.example.realtime_db
+
+import android.app.ProgressDialog
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.realtime_db.databinding.ActivityMainBinding
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+    lateinit var progressDialog: ProgressDialog
+
+    val db = Firebase.firestore
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+ binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val myRef = database.getReference()
+        var count = 0
+        val name = binding.addName.text.toString()
+        val id = binding.addId.text.toString()
+        val age = binding.addAge.text.toString()
+
+binding.btnAddUser.setOnClickListener {
+    val person = hashMapOf(
+        "name" to name ,
+        "id" to id ,
+        "age" to age
+    )
+    myRef.child("person").child("$count").setValue(person)
+    count++
+    Toast.makeText(this,"Success",Toast.LENGTH_SHORT).show()
+}
+
+binding.getAddUser.setOnClickListener {
+
+    myRef.addValueEventListener(object: ValueEventListener {
+
+        override fun onDataChange(snapshot: DataSnapshot) {
+            val value = snapshot.getValue()
+            binding.viewData.text = value.toString()
+            Toast.makeText(this,"Success",Toast.LENGTH_SHORT).show()
+
+        }
+
+        override fun onCancelled(error: DatabaseError) {
+            Toast.makeText(this,"Error",Toast.LENGTH_SHORT).show()
+
+        }
+
+    })
+
+}
+
+    }
+}
